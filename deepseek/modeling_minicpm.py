28018811e7e449e48a566fef71898b0597ac0209
onenewcode <lovestudy@qq.com>
2025-02-10 11:20:20 +0000

    t

--- deepseek/modeling_minicpm.py
+++ deepseek/modeling_minicpm.py
@@ -235,10 +235,6 @@ class MiniCPMLongRoPE(MiniCPMRotaryEmbedding):
             self.inv_freq.to(device=device).to(dtype)
         )
         
-        # freqs = torch.outer(t, self.inv_freq)
-        # # Different from paper, but it uses a different permutation in order to obtain the same calculation
-        # emb = torch.cat((freqs, freqs), dim=-1)
-
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
         emb = torch.cat((freqs, freqs), dim=-1)
         self.register_buffer("cos_cached", emb.cos().to(dtype) * self.scaling_factor, persistent=False)
